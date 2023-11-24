f70f97ede4c4c519f983441ea6459d6dde5af769
Rishabh <iamrishabhnirmalkar@gmail.com>
2023-11-24 21:46:06 +0530

    promise

--- Day05/02Promise.js
+++ Day05/02Promise.js
@@ -1,4 +1,24 @@
 //+++++++++++++Promise01
 
-const promise01 = new Promise();
-console.log(promise01);
+const promise01 = new Promise(function (resolve, reject) {
+  //DO an asyn task
+  //DB call , cryptography, network
+  setTimeout(function () {
+    console.log(`Async task is compelete`);
+    resolve();
+  }, 1000);
+});
+
+promise01.then(function () {
+  console.log("Promise consumed");
+});
+
+//+++++++++
+new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Async task 2");
+    resolve();
+  }, 1000);
+}).then(function () {
+  console.log("Asyn 2 resolve");
+});
