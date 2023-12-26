83e5296e97580c9859f36a2637e2a598bbfe3db0
Rishabh <iamrishabhnirmalkar@gmail.com>
2023-12-26 13:47:52 +0530

    promise

--- Day05/02Promise.js
+++ Day05/02Promise.js
@@ -85,6 +85,18 @@
 
 ////++++++++++++
 
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// }
+/////++++++++++++++++
+
 // fetch("https://api.github.com/users/hiteshchoudhary")
 //   .then((response) => {
 //     return response.json();
